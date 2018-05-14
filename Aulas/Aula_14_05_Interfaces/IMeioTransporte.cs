f6c07010941fbee22c95485c8ea2a2a5973b9988
TiranoMax <alexsandroarents@hotmail.com>
2018-05-14 08:46:14 -0300

    Criando Interface IMeioTransporte

---
+++ Aulas/Aula_14_05_Interfaces/IMeioTransporte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_14_05_Interfaces
+{
+    interface IMeioTransporte
+    {
+
+        void Acelerar();
+        void Desacelerar();
+
+
+    }
+}
