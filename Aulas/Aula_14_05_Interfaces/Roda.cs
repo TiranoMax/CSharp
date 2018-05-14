b312c4a07fbd1e0bfd841aeb028d46a478660c8c
TiranoMax <alexsandroarents@hotmail.com>
2018-05-14 11:03:50 -0300

    Criado a classe Roda

---
+++ Aulas/Aula_14_05_Interfaces/Roda.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Aula_14_05_Interfaces
+{
+    class Roda
+    {
+        public int RodaId { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int Marca { get; set; }
+
+        public Marca _Marca { get; set; }
+    }
+}
