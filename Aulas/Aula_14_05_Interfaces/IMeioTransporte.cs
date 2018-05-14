ced71bba903e7a752bb224e81c9eb6034e834215
TiranoMax <alexsandroarents@hotmail.com>
2018-05-14 09:27:52 -0300

    Retirado usings desnecessarios da Interface IMeioTransporte

--- Aulas/Aula_14_05_Interfaces/IMeioTransporte.cs
+++ Aulas/Aula_14_05_Interfaces/IMeioTransporte.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Aula_14_05_Interfaces
 {
     interface IMeioTransporte
