aae9aebd81f4d35aa0e8fcdf0828d5ad7ce834b8
TiranoMax <alexsandroarents@hotmail.com>
2018-05-14 08:53:52 -0300

    Implementado IMeioTrasporte

---
+++ Aulas/Aula_14_05_Interfaces/Carro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Aula_14_05_Interfaces
+{
+    class Carro : IMeioTransporte
+    {
+        public int Velocidade { get; set; }
+
+        public Carro()
+        {
+            Velocidade = 0;
+        }
+
+        public void Acelerar()
+        {
+            Velocidade += 10;
+        }
+
+        public void Desacelerar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
