1a22073bef67ff28950362b366e4a18e563449ce
TiranoMax <alexsandroarents@hotmail.com>
2018-05-17 08:46:12 -0300

    Criado a class (Atividade) com seus atributos

---
+++ Aulas/Aula_17_05_Camadas/Model/Atividade.cs
@@ -0,0 +1,11 @@
+﻿namespace Aula_17_05_Camadas.Model
+{
+    class Atividade
+    {
+        public int AtividadeId { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
