4de15185a0bf4f3c6da3e45db0e2634ed15cdd0c
TiranoMax <alexsandroarents@hotmail.com>
2018-05-17 09:19:15 -0300

    Criado o método ExibirMenu no (AtividadeView)

--- Aulas/Aula_17_05_Camadas/View/AtividadeView.cs
+++ Aulas/Aula_17_05_Camadas/View/AtividadeView.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula_17_05_Camadas.View
 {
     class AtividadeView
     {
+
+        public int ExibirMenu()
+        {
+            Console.WriteLine("========================");
+            Console.WriteLine("= Escolha uma opção:   =");
+            Console.WriteLine("= 1) Criar Atividade   =");
+            Console.WriteLine("= 2) Listar Atividade  =");
+            Console.WriteLine("= 3) Buscar Atividade  =");
+            Console.WriteLine("= 4) Editar Atividade  =");
+            Console.WriteLine("= 5) Excluir Atividade =");
+            Console.WriteLine("========================");
+
+            int opcao = int.Parse(Console.ReadLine());
+            return opcao;
+        }
+
     }
 }
