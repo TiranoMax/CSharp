be0dfaf0fee41309d01ed8d56bf1536c994f210c
TiranoMax <alexsandroarents@hotmail.com>
2018-05-17 08:40:38 -0300

    Criacao do projeto Aula_17_05_Camadas e seus folders

---
+++ Aulas/Aula_17_05_Camadas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_17_05_Camadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
