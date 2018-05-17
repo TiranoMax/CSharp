93215f1618185aa75ddb2d5840c1893200de9164
TiranoMax <alexsandroarents@hotmail.com>
2018-05-17 09:12:31 -0300

    Criado a Classe (AtividadeView)

---
+++ Aulas/Aula_17_05_Camadas/View/AtividadeView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_17_05_Camadas.View
+{
+    class AtividadeView
+    {
+    }
+}
