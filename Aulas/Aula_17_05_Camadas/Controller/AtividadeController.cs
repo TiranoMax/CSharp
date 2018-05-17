e56338c9fc04549c74a1feaa3587e3970b11896c
TiranoMax <alexsandroarents@hotmail.com>
2018-05-17 08:46:42 -0300

    Criado a classe (AtividadeController)

---
+++ Aulas/Aula_17_05_Camadas/Controller/AtividadeController.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula_17_05_Camadas.Controller
+{
+    class AtividadeController
+    {
+
+        //simulando tabela
+        public List<Atividade> ListarAtividades { get; set; }
+        //Salvar
+        //Listar
+        //Ler
+        //Editar
+        //Excluir
+    }
+}
